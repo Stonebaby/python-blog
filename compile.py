b7fb37c624ab2798f0d7eadb8efdf2bf599fa379
Stonebaby <Erin@Zacks-MacBook-Air.local>
2014-02-17 20:25:32 +1100

    update compile

--- compile.py
+++ compile.py
@@ -1,35 +1,10 @@
+'''import _config in site.py'''
 
-'''import title'''
-import _config
-SITE_TITLE = '<title>'+ _config.title + '</title>'
-print SITE_TITLE
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.insert(0, currentdir) 
 
-'''put all source files into one list, open and merge them together'''
-file_list = ["_templates/header.html", "_templates/body.html"]
-
-def merge_file(file_list):
-	content = ''
-	for file_name in file_list:
-		with open(file_name, 'r') as fil:
-			for line in fil:
-				content += line
-	return content
+from _includes.site import Site
 	
-f_merge = merge_file(file_list)
-
-'''insert title in f_merge, between the <head> '''
-def insert(str):
-	f_merge_spl = f_merge.split()
-	for index, item in enumerate(f_merge_spl):
-		if item == '<head>':
-			f_merge_spl.insert( index + 1, SITE_TITLE )
-	return ''.join(f_merge_spl)
-	
-f_merge_title = insert(f_merge)	
-
-'''write into _site/index.html'''
-output = open('_site/index.html','w')
-output.write("<!DOCTYPE html>\n<html>\n%s</html>" % f_merge_title)
-
-
-
+content = Site()
+content.compile()
